305d29b1e9bc781e296f93c313e1ac4268fa1bbe
Bogdan Mart <mart.bogdan@gmail.com>
2018-08-28 17:38:37 +0300

    make script fail more early, if not all args are present

--- entry-point.sh
+++ entry-point.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-aws configure set aws_access_key_id $1
-aws configure set aws_secret_access_key $2
-
+aws configure set aws_access_key_id $1 && \
+aws configure set aws_secret_access_key $2 && \
 aws ecr get-login --no-include-email --region $3
